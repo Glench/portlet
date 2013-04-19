bdebdf747a39300337f255b908d3da48c6c3f50e
Glen Chiacchieri <www@glench.com>
2013-04-19 15:02:31 -0400

    remove forms support, get/post args

--- portlet/portlet.py
+++ portlet/portlet.py
@@ -6,6 +6,7 @@ class BasePortlet(object):
         self.template = template
 
     def execute(self):
+        """Function for whether this portlet should render."""
         return True
   
     def __call__(self, render_func):
@@ -20,17 +21,15 @@ class BasePortlet(object):
 class Portlet(BasePortlet):
     PREFIX_CSS_CLASS = 'portlet'
     """Base Class for generating chunks of HTML content"""
-    def __init__(self, template, css_class=None, get_args=None, post_args=None):
+    def __init__(self, template, css_class=None):
         css_class = css_class or []
         if isinstance(css_class, basestring):
             css_class = [css_class.split(' ')]
 
-        self.get_args = get_args or {}
-        self.post_args = post_args or {}
         self.css_class = set(css_class)
         self._id = self.html_id()
 
-        BasePortlet.__init__(self, template)
+        super(BasePortlet, self).__init__(template)
 
     def _execution_complete(self, render_func):
         content = BasePortlet._execution_complete(self, render_func)
@@ -40,20 +39,6 @@ class Portlet(BasePortlet):
             css_class = '{} {}' % (css_class, ' '.join(self.css_class))
 
         return '<div class="{}" id="{}">{}</div>'.format(css_class, self._id, content)
-  
-    def forms(self):
-        for form in self._forms():
-            result = form.process_strings(self.get_args, self.post_args)
-            if result != None:
-                return result
-
-        return False
-
-    def _forms(self):
-        """
-        Override this. Return a list of forms that should be run.
-        """
-        return []
 
     @classmethod
     def html_id(cls, prefix='portlet_'):
@@ -68,17 +53,3 @@ class AsyncPortlet(Portlet):
                 callback('')
 
         self.execute(execute_cb)
-
-    def forms(self, callback):
-        forms = self.forms()
-        if len(forms) == 0:
-            callback(None)
-            return
-
-        for form in forms:
-            if isinstance(form, AsyncForm):
-                form.process_strings(self.get_args, self.post_args, callback)
-            else:
-                callback(form.process_strings(self.get_args, self.post_args))
-
-class AsyncForm(object): pass
